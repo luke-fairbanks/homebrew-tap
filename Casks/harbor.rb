16e47cede12d74adeb94f707562706e582d0dd91
Luke Fairbanks <lukedfairbanks@gmail.com>
2026-07-13 13:10:20 -0600

    harbor 0.4.1

--- Casks/harbor.rb
+++ Casks/harbor.rb
@@ -1,13 +1,13 @@
 cask "harbor" do
-  version "0.1.0"
-  sha256 "53baea57fe3ac005049d1fc7adcc0d60acabca6990dce9898a3436d2a2501c62"
+  version "0.4.1"
+  sha256 "1bdd6b899e64371275bcca004175472dfde47981c6da0b474ca976547ad5c524"
 
   url "https://github.com/luke-fairbanks/harbor-mcp/releases/download/v#{version}/Harbor_#{version}_universal.dmg"
   name "Harbor"
-  desc "One-button local dev server orchestration, MCP-native"
+  desc "Discover and orchestrate local development servers"
   homepage "https://github.com/luke-fairbanks/harbor-mcp"
 
-  depends_on :macos
+  depends_on macos: :big_sur
 
   app "Harbor.app"
 
